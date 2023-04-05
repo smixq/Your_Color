1b43a5a5eb42191ba17c17f0df8e3df4b9d42b1b
smixq <csvaqlik@gmail.com>
2023-04-05 18:00:43 +0300

    test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request, make_response, session
 
 from data import db_session
-# from form.login import LoginForm
+from form.login import LoginForm
 # from form.register import RegisterForm
 from flask_login import LoginManager, login_user
 
@@ -18,6 +18,13 @@ def main():
 def index():
     params = {'link': 'css/styles_for_generate_plattes.css'}
     return render_template("generate_plattes.html", **params)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template('login.html', title='Авторизация', form=form, link = )
 
     
 if __name__ == '__main__':
