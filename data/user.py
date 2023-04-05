b18eee83c6391c105446342c68e5ff6ee77d6672
smixq <csvaqlik@gmail.com>
2023-04-05 19:48:44 +0300

    БЫСТРО

--- data/user.py
+++ data/user.py
@@ -1,15 +1,18 @@
 import datetime
 import sqlalchemy
+from flask_login import UserMixin
+
 from .db_session import SqlAlchemyBase
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    avatar = sqlalchemy.Column(sqlalchemy.BLOB, index=True, unique=True, nullable=True)
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
--- main.py
+++ main.py
@@ -8,6 +8,14 @@ from data.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asffsdfSDFASFKJFSADHFGJSDJFG'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 def main():
@@ -27,10 +35,12 @@ def login():
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user:
-            if user.check_password(form.password.data):
-                print(11213123)
-                return redirect('/')
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 
@@ -55,8 +65,6 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
-
-
 
 
 if __name__ == '__main__':
