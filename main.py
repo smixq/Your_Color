58484843d8a671578b560243b077aa1247fca7b9
smixq <csvaqlik@gmail.com>
2023-04-05 18:58:15 +0300

    testpushfor pull

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, redirect, request, make_response, sess
 
 from data import db_session
 from form.login import LoginForm
-# from form.register import RegisterForm
+from form.register import RegisterForm
 from flask_login import LoginManager, login_user
+from data.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asffsdfSDFASFKJFSADHFGJSDJFG'
@@ -19,13 +20,37 @@ def index():
     params = {'link': 'css/styles_for_generate_plattes.css'}
     return render_template("generate_plattes.html", **params)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         return redirect('/')
-    return render_template('login.html', title='Авторизация', form=form, link = )
+    return render_template('login.html', title='Авторизация', form=form)
 
-    
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 if __name__ == '__main__':
     main()
