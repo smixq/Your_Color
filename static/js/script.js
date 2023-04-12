f626095a74cdd1cef7fdb1cd4fe69cfc8ea6bb75
smixq <csvaqlik@gmail.com>
2023-04-12 19:42:56 +0300

    асинхрность

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+let likes = document.querySelectorAll('span')
+
+likes.forEach((like)=>{
+    like.addEventListener('click', (event) => {
+        let el = event.target
+        let i = parseInt(el.textContent)
+        el.textContent = i += 1
+        let xhr = new XMLHttpRequest();
+        let id = el.dataset.id
+        xhr.open("POST", `sute.ru/add-like`)
+        xhr.send({id: id, user_id: user_id})
+    })
+})
